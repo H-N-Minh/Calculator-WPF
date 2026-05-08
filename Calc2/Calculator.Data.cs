a1dda4e1cf12ed034a36d5e9a5bdd20272cbdd30
Minh Hoang <sdk178@sdk.at>
2026-05-08 13:17:54 +0200

    done CanExecute. next is execute

---
+++ Calc2/Calculator.Data.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Windows.Input;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Calc2;
+
+public enum State
+{
+    Start, Equal, Number, Float, Operator
+}
+
+public partial class Calculator : INotifyPropertyChanged
+{
+    // Fields
+    private decimal result = 0;
+
+    // Properties
+    private State state = State.Start;
+    public State State
+    {
+        get { return state; }
+        set { state = value; }
+    }
+
+
+    private Operator currentOperator = Operator.Plus;
+    public Operator CurrentOperator
+    {
+        get { return currentOperator; }
+        set
+        {
+            currentOperator = value;
+            OnPropertyChanged(nameof(CurrentOperator));
+        }
+    }
+
+    private int exponent;
+    public int Exponent
+    {
+        get { return exponent; }
+        set
+        {
+            exponent = value;
+            OnPropertyChanged(nameof(Exponent));
+            OnPropertyChanged(nameof(Display));
+        }
+    }
+
+    private decimal buffer;       
+    public decimal Buffer
+    {
+        get { return buffer; }
+        set
+        {                       // Notify changes to buffer (also Display)
+            buffer = value;
+            OnPropertyChanged(nameof(Buffer));
+            OnPropertyChanged(nameof(Display));
+        }
+    }
+
+    public string Display       // Readonly, string version of buffer
+    {
+        get
+        {
+            return Exponent switch
+            {
+                -1 => buffer.ToString() + ",",
+                _ => buffer.ToString()
+            };
+        }
+    }
+
+    public CommandHandler CommandHandler { get; set; }  
+
+    // Event
+    public event PropertyChangedEventHandler? PropertyChanged;
+
+
+    /* ########################################################## */
+    // Ctor
+    public Calculator()
+    {
+        CommandHandler = new CommandHandler();
+    }
+
+    // Method
+    private void OnPropertyChanged(string propertyName)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+
+}
+
+public class CommandHandler : ICommand
+{
+    // Fields
+    private Calculator calculator;
+
+    // Ctor
+    public CommandHandler(Calculator calc)
+    {
+        calculator = calc;
+    }
+
+    // Event
+    public event EventHandler? CanExecuteChanged;
+
+    // ICommand implementation
+    public bool CanExecute(object? parameter)
+    {
+        string command = parameter as string ?? throw new ArgumentException("Parameter must be a string", nameof(parameter));
+
+        switch (command)
+        {
+            case "C": case "+/-": case "0": case "1": case "2": case "3": case "4": case "5": case "6": case "7": case "8": case "9":
+                break;
+            case "BackSpace": case "CE": case "=":
+                if (calculator.State == State.Operator) return false;
+                break;
+            case "*": case "/": case "-": case "+":
+                if (calculator.State == State.Start) return false;
+                break;
+            case ",":
+                if (calculator.State == State.Float) return false;
+                break;
+            default:
+                throw new ArgumentException("Not implemented command", nameof(parameter));
+        }
+
+        return true;
+    }
+
+    public void Execute(object? parameter)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void RaiseCanExecuteChanged()
+    {
+        CanExecuteChanged?.Invoke(this, EventArgs.Empty);
+    }
+}
---
+++ Calc2/Calculator.Logic.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Calc2;
+
+public enum Operator
+{
+    Plus, Minus, Divide, Multiply
+}
+
+public partial class Calculator
+{
+    private void enterEqual(object? parameter)
+    {
+        Buffer = currentOperator switch
+        {
+            Operator.Plus => result + buffer,
+            Operator.Minus => result - buffer,
+            Operator.Multiply => result * buffer,
+            Operator.Divide => result / buffer,
+            _ => result
+        };
+        result = 0;
+        exponent = 0;
+        CurrentOperator = Operator.Plus;
+        isTyping = true;
+    }
+    private bool isStartState(object? parameter)
+    {
+        return result == 0 && buffer == 0;
+    }
+    private void enterOperator(object? parameter)
+    {
+        string param = parameter?.ToString() ?? "+";
+        if (IsTyping)
+        {
+            result = currentOperator switch
+            {
+                Operator.Plus => result + buffer,
+                Operator.Minus => result - buffer,
+                Operator.Multiply => result * buffer,
+                Operator.Divide => result * buffer,
+                _ => result
+            };
+            buffer = 0;
+            exponent = 0;
+        }
+
+        currentOperator = param switch
+        {
+            "+" => Operator.Plus,
+            "-" => Operator.Minus,
+            "*" => Operator.Multiply,
+            "/" => Operator.Divide,
+            _ => Operator.Plus
+        };
+
+        IsTyping = false;
+    }
+    private void enterClearAll(object? parameter)
+    {
+        result = 0;
+        Buffer = 0;
+        Exponent = 0;
+        CurrentOperator = Operator.Plus;
+        IsTyping = false;
+    }
+    private void enterBackSpace(object? parameter)
+    {
+        string number = Display;
+        // delete last digit
+        if (!string.IsNullOrEmpty(number))
+        {
+            number = number[..^1];
+        }
+
+        // if theres no digit left
+        if (string.IsNullOrEmpty(number))
+        {
+            Buffer = 0;
+            Exponent = 0;
+            isTyping = false;
+            return;
+        }
+
+        // else theres still digit left
+        if (Exponent < 0) Exponent++;
+        if (Exponent == -1)
+        {
+            number = number[..^1];
+        }
+
+        Buffer = decimal.Parse(number);
+    }
+    private void enterComma(object? parameter)
+    {
+        Exponent--;
+        IsTyping = true;
+    }
+    private void enterDigit(object? parameter)
+    {
+        Buffer = Exponent switch
+        {
+            0 => Buffer * 10 + decimal.Parse(parameter?.ToString() ?? "0"),
+            _ => Buffer + decimal.Parse(parameter?.ToString() ?? "0") * (decimal)Math.Pow(10, Exponent--)
+        };
+        IsTyping = true;
+    }
+
+}
