b7a57735b78264fc01645f2c6c685be2d988707b
Minh Hoang <sdk178@sdk.at>
2026-05-08 16:45:54 +0200

    Equal and operator working flawlessly

---
+++ Calc2/Calculator.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Windows.Input;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Calc2;
+
+public enum State
+{
+    Start, Equal, Number, Float, Operator
+}
+
+public partial class Calculator : INotifyPropertyChanged
+{
+    // Fields
+    private decimal result = 0;
+
+    // Properties
+    private State currentState = State.Start;
+    public State CurrentState
+    {
+        get { return currentState; }
+        set { 
+            currentState = value;
+            OnPropertyChanged(nameof(CurrentState));
+            Handler.RaiseCanExecuteChanged();
+            OnPropertyChanged(nameof(Display));
+        }
+    }
+
+
+    private Operator currentOperator = Operator.Plus;
+    public Operator CurrentOperator
+    {
+        get { return currentOperator; }
+        set
+        {
+            currentOperator = value;
+            OnPropertyChanged(nameof(CurrentOperator));
+        }
+    }
+
+    private int exponent;
+    public int Exponent
+    {
+        get { return exponent; }
+        set
+        {
+            exponent = value;
+            OnPropertyChanged(nameof(Exponent));
+            OnPropertyChanged(nameof(Display));
+        }
+    }
+
+    private decimal buffer;       
+    public decimal Buffer
+    {
+        get { return buffer; }
+        set
+        {                       // Notify changes to buffer (also Display)
+            buffer = value;
+            OnPropertyChanged(nameof(Buffer));
+            OnPropertyChanged(nameof(Display));
+        }
+    }
+
+    public string Display       // Readonly, string version of buffer or result
+    {
+        get
+        {
+            if (CurrentState == State.Equal)
+            {
+                return result.ToString();
+            }
+            return Exponent switch
+            {
+                -1 => buffer.ToString() + ",",
+                _ => buffer.ToString()
+            };
+        }
+    }
+
+    public CommandHandler Handler { get; set; }  
+
+    // Event
+    public event PropertyChangedEventHandler? PropertyChanged;
+
+
+    /* ########################################################## */
+    // Ctor
+    public Calculator()
+    {
+        Handler = new CommandHandler(this);
+    }
+
+    // Method
+    private void OnPropertyChanged(string propertyName)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+
+    /* ########################################################## */
+    // Nested Class
+    public class CommandHandler : ICommand
+    {
+        // Fields
+        private Calculator calculator;
+
+        // Ctor
+        public CommandHandler(Calculator calc)
+        {
+            calculator = calc;
+        }
+
+        // ICommand implementation
+        public event EventHandler? CanExecuteChanged;
+
+        public bool CanExecute(object? parameter)
+        {
+            string command = parameter as string ?? throw new ArgumentException("Parameter must be a string", nameof(parameter));
+
+            switch (command)
+            {
+                case "C": case "+/-": case "0": case "1": case "2": case "3": case "4": case "5": case "6": case "7": case "8": case "9":
+                    break;
+                case "=":
+                    if (calculator.CurrentState == State.Operator || calculator.CurrentState == State.Start) return false;
+                    // Check for division by zero
+                    if (calculator.CurrentOperator == Operator.Divide && calculator.Buffer == 0)
+                    {
+                        return false;
+                    }
+                    break;
+                case "BackSpace": case "CE": 
+                    if (calculator.CurrentState == State.Operator) return false;
+                    break;
+                case "*": case "/": case "-": case "+":
+                    if (calculator.CurrentState == State.Start) return false;
+                    break;
+                case ",":
+                    if (calculator.CurrentState == State.Float) return false;
+                    break;
+                default:
+                    throw new ArgumentException("Not implemented command", nameof(parameter));
+            }
+
+            return true;
+        }
+
+        public void Execute(object? parameter)
+        {
+            string command = parameter as string ?? throw new ArgumentException("Parameter must be a string", nameof(parameter));
+
+            switch (command)
+            {
+                case "0": case "1": case "2": case "3": case "4": case "5": case "6": case "7": case "8": case "9":
+                    calculator.EnterDigit(int.Parse(command)); 
+                    break;
+                case "C":
+                    calculator.EnterClearAll();
+                    break;
+                case "=":
+                    calculator.EnterEqual();
+                    break;
+                //case "+/-": 
+                //    break;
+                //case "BackSpace": case "CE":
+                //    if (calculator.State == State.Operator) return false;
+                //    break;
+                case "*": case "/": case "-": case "+":
+                    calculator.EnterOperator(command);
+                    break;
+                case ",":
+                    calculator.EnterComma();
+                    break;
+                default:
+                    throw new ArgumentException("Not implemented command", nameof(parameter));
+            }
+        }
+
+        public void RaiseCanExecuteChanged()
+        {
+            CanExecuteChanged?.Invoke(this, EventArgs.Empty);
+        }
+    }
+}
---
+++ Calc2/CommandHandler.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+using System.Threading.Channels;
+
+namespace Calc2;
+
+public enum Operator
+{
+    Plus, Minus, Multiply, Divide
+}
+
+public partial class Calculator
+{
+    private void EnterEqual()
+    {
+        Debug.Assert(CurrentState != State.Operator, "Equal cant be entered after an operator");
+        Debug.Assert(CurrentState != State.Start, "Equal cant be entered at the start");
+        Debug.Assert(!(CurrentOperator == Operator.Divide && Buffer == 0), "Equal cant be entered for division by 0");
+
+        // If only 1 number entered, pressing equal repetively should not change result.
+        bool isOnlyBufferHasNumber = result == 0 && CurrentOperator == Operator.Plus && (CurrentState == State.Number || CurrentState == State.Float);
+
+        // Updating fields and properties
+        result = CalculateResult();
+        Exponent = 0;
+        if (isOnlyBufferHasNumber) Buffer = 0;
+        CurrentState = State.Equal;
+    }
+
+    private void EnterOperator(string arithOperator)
+    {
+        Debug.Assert(CurrentState != State.Start, "Operator cant be entered at the start");
+
+        // If operator is first entered after a number, calculate the result of the previous operation
+        if (CurrentState == State.Number || CurrentState == State.Float)
+        {
+            result = CalculateResult();
+        }
+
+        Buffer = 0;
+        Exponent = 0;
+        CurrentOperator = arithOperator switch
+        {
+            "+" => Operator.Plus,
+            "-" => Operator.Minus,
+            "*" => Operator.Multiply,
+            "/" => Operator.Divide,
+            _ => throw new ArgumentException("arithOperator is not an operator", nameof(arithOperator))
+        };
+        CurrentState = State.Operator;
+    }
+
+    private void EnterClearAll()
+    {
+        result = 0;
+        Buffer = 0;
+        Exponent = 0;
+        CurrentOperator = Operator.Plus;
+        CurrentState = State.Start;
+    }
+    //private void EnterBackSpace(object? parameter)
+    //{
+    //    string number = Display;
+    //    // delete last digit
+    //    if (!string.IsNullOrEmpty(number))
+    //    {
+    //        number = number[..^1];
+    //    }
+
+    //    // if theres no digit left
+    //    if (string.IsNullOrEmpty(number))
+    //    {
+    //        Buffer = 0;
+    //        Exponent = 0;
+    //        isTyping = false;
+    //        return;
+    //    }
+
+    //    // else theres still digit left
+    //    if (Exponent < 0) Exponent++;
+    //    if (Exponent == -1)
+    //    {
+    //        number = number[..^1];
+    //    }
+
+    //    Buffer = decimal.Parse(number);
+    //}
+    private void EnterComma()
+    {
+        Debug.Assert(CurrentState != State.Float, "Comma cant be added to a float");
+        Exponent--;
+        CurrentState = State.Float;
+    }
+    private void EnterDigit(decimal digit)
+    {
+        Buffer = Exponent switch
+        {
+            0 => Buffer * 10 + digit,
+            _ => Buffer + digit * (decimal)Math.Pow(10, Exponent--)
+        };
+
+        if (CurrentState != State.Float)
+        {
+            CurrentState = State.Number;
+        }
+        else
+        {
+            CurrentState = State.Float;
+        }
+    }
+
+    private decimal CalculateResult()
+        /* Update the Result by doing the calculation with the buffer using the current operator */
+    {
+        return CurrentOperator switch
+        {
+            Operator.Plus => result + buffer,
+            Operator.Minus => result - buffer,
+            Operator.Multiply => result * buffer,
+            Operator.Divide => result / buffer,
+            _ => throw new InvalidOperationException("CurrentOperator is not a valid math operator")
+        };
+    }
+}
